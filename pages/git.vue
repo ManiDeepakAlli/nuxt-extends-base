1d42261086c86e5bbb293162378c1ab479f43448
ManiDeepakAlli <dmani533@gmail.com>
2024-01-23 06:34:58 -0500

    Update git.vue

--- pages/git.vue
+++ pages/git.vue
@@ -1,20 +1,25 @@
+<!-- MainComponent.vue -->
 <template>
   <div class="home">
     <h1>Welcome to Our Ecommerce Store</h1>
     <div class="product-list">
-      <div v-for="product in products" :key="product.id" class="product">
-        <img :src="product.image" alt="Product Image" class="product-image" />
-        <h2>{{ product.name }}</h2>
-        <p>{{ product.description }}</p>
-        <p class="price">{{ product.price }} USD</p>
-        <button @click="addToCart(product)">Add to Cart</button>
-      </div>
+      <product-card
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+        :add-to-cart="addToCart"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import ProductCard from './ProductCard.vue';
+
 export default {
+  components: {
+    ProductCard,
+  },
   data() {
     return {
       products: [
@@ -23,14 +28,14 @@ export default {
           name: 'Product 1',
           description: 'Description for Product 1',
           price: 19.99,
-          image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
+          image: 'path/to/product1-image.jpg',
         },
         {
           id: 2,
           name: 'Product 2',
           description: 'Description for Product 2',
           price: 29.99,
-          image: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg',
+          image: 'path/to/product2-image.jpg',
         },
         // Add more products as needed
       ],
@@ -55,22 +60,5 @@ export default {
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
-}
-
-.product {
-  border: 1px solid #ccc;
-  padding: 10px;
-  margin: 10px;
-  max-width: 200px;
-}
-
-.product-image {
-  max-width: 100%;
-  height: auto;
-}
-
-.price {
-  font-weight: bold;
-  color: green;
 }
 </style>
