c2c641d27deffe0bf08aa18dc2a32885cb8ec80e
ManiDeepakAlli <dmani533@gmail.com>
2024-01-23 06:45:23 -0500

    Create HeaderComponent.vue

---
+++ components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<!-- HeaderComponent.vue -->
+<template>
+  <header class="header">
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.header {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  text-align: center;
+}
+</style>
