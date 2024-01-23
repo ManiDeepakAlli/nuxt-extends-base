6469d9a71b4322404771a3afaf49cd48b3c5cbc5
ManiDeepakAlli <dmani533@gmail.com>
2024-01-23 06:46:08 -0500

    Create FooterComponent.vue

---
+++ components/FooterComponent.vue
@@ -0,0 +1,18 @@
+<!-- FooterComponent.vue -->
+<template>
+  <footer class="footer">
+    <p>&copy; 2024 Your Ecommerce Store. All rights reserved.</p>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #333;
+  color: white;
+  padding: 10px;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+}
+</style>
